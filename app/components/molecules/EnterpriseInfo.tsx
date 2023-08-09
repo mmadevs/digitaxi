b1c7dadbdfd82cc79efeb20735ac137666ccffc4
MMA Devs <mmadevsys@gmail.com>
2023-08-09 17:51:19 -0300

    fix: hyperlink was not working correctly

--- app/components/molecules/EnterpriseInfo.tsx
+++ app/components/molecules/EnterpriseInfo.tsx
@@ -3,8 +3,6 @@ import Text from '../atoms/Text'
 import Image from 'next/image'
 import { GiPhone } from 'react-icons/gi'
 import { FiAtSign } from 'react-icons/fi'
-import Link from 'next/link'
-import { isMobile } from 'react-device-detect'
 const EnterpriseInfo: FunctionComponent = () => {
 	return (
 		<div className='text-center flex flex-col md:items-end items-center min-h-[4rem]'>
@@ -16,20 +14,20 @@ const EnterpriseInfo: FunctionComponent = () => {
 					fill
 				/>
 			</div>
-			<Link
-				href={isMobile ? 'tel:+557136251725' : '#'}
+			<a
+				href={'tel:+557136251725'}
 				className='flex text-sm items-center gap-1'
 			>
 				<GiPhone className='text-sm' />
 				<Text type='smaller'>(71) 3625-1725</Text>
-			</Link>
-			<Link
-				href={isMobile ? 'mailto:atendimento@digitaxi.coop.br' : '#'}
+			</a>
+			<a
+				href={'mailto:atendimento@digitaxi.coop.br'}
 				className='flex text-sm items-center gap-1'
 			>
 				<FiAtSign className='text-sm' />
 				<Text type='smaller'>atendimento@digitaxi.coop.br</Text>
-			</Link>
+			</a>
 			<Text type='smaller'>Todos os direitos reservados | 2023</Text>
 		</div>
 	)
--- app/components/molecules/HeaderTopInfo.tsx
+++ app/components/molecules/HeaderTopInfo.tsx
@@ -2,8 +2,6 @@ import { FunctionComponent } from 'react'
 import { GiPhone } from 'react-icons/gi'
 import { FiAtSign } from 'react-icons/fi'
 import Text from '../atoms/Text'
-import Link from 'next/link'
-import { isMobile } from 'react-device-detect'
 
 const HeaderTopInfo: FunctionComponent = () => {
 	return (
@@ -11,20 +9,20 @@ const HeaderTopInfo: FunctionComponent = () => {
 			<Text className='max-md:hidden font-bold' type='smaller'>
 				Fale conosco!
 			</Text>
-			<Link
-				href={isMobile ? 'tel:+557136251725' : '#'}
+			<a
+				href={'tel:+557136251725'}
 				className='flex text-sm items-center gap-1'
 			>
 				<GiPhone />
 				<Text type='smaller'>(71) 3625-1725</Text>
-			</Link>
-			<Link
-				href={isMobile ? 'mailto:atendimento@digitaxi.coop.br' : '#'}
+			</a>
+			<a
+				href={'mailto:atendimento@digitaxi.coop.br'}
 				className='flex text-sm items-center gap-1'
 			>
 				<FiAtSign />
 				<Text type='smaller'>atendimento@digitaxi.coop.br</Text>
-			</Link>
+			</a>
 		</header>
 	)
 }
